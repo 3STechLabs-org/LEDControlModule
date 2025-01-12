288386b91435a39329c0967d228dee1951d3eea5
Azhar Ali Khan <azhar987k@gmail.com>
2025-01-12 18:21:27 +0500

    Added LEDControlModule

---
+++ LEDControlModule.cpp
@@ -0,0 +1,77 @@
+#include "LEDControlModule.h"
+
+namespace emsesp {
+
+LEDControlModule::LEDControlModule()
+    : strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800), ledConfigs(NUM_LEDS) {
+    strip.begin();
+    strip.show(); // Initialize all LEDs to off
+}
+
+void LEDControlModule::start(emsesp::EMSESP* emsesp_main, bool test_mode) {
+    updateLEDs();
+    EMSESP::logger().info("LEDControlModule started");
+}
+
+void LEDControlModule::loop() {
+    handleBlink();
+}
+
+void LEDControlModule::configureLED(size_t index, uint8_t r, uint8_t g, uint8_t b, bool blink) {
+    if (index >= NUM_LEDS) {
+        EMSESP::logger().error("Invalid LED index: %d", index);
+        return;
+    }
+
+    ledConfigs[index].r = r;
+    ledConfigs[index].g = g;
+    ledConfigs[index].b = b;
+    ledConfigs[index].blink = blink;
+
+    updateLEDs();
+}
+
+void LEDControlModule::list(JsonObject output) {
+    for (size_t i = 0; i < NUM_LEDS; ++i) {
+        char ledKey[8];
+        snprintf(ledKey, sizeof(ledKey), "led%d", static_cast<int>(i + 1));
+
+        JsonObject led = output.createNestedObject(ledKey);
+        led["r"] = ledConfigs[i].r;
+        led["g"] = ledConfigs[i].g;
+        led["b"] = ledConfigs[i].b;
+        led["blink"] = ledConfigs[i].blink;
+    }
+}
+
+void LEDControlModule::setLED(size_t index, uint8_t r, uint8_t g, uint8_t b) {
+    strip.setPixelColor(index, strip.Color(r, g, b));
+}
+
+void LEDControlModule::updateLEDs() {
+    for (size_t i = 0; i < NUM_LEDS; ++i) {
+        setLED(i, ledConfigs[i].r, ledConfigs[i].g, ledConfigs[i].b);
+    }
+    strip.show();
+}
+
+void LEDControlModule::handleBlink() {
+    unsigned long currentTime = millis();
+    if (currentTime - lastBlinkTime >= 500) { // 1 Hz (500 ms on/off)
+        lastBlinkTime = currentTime;
+        blinkState = !blinkState;
+
+        for (size_t i = 0; i < NUM_LEDS; ++i) {
+            if (ledConfigs[i].blink) {
+                if (blinkState) {
+                    setLED(i, ledConfigs[i].r, ledConfigs[i].g, ledConfigs[i].b);
+                } else {
+                    setLED(i, 0, 0, 0); // Turn off the LED
+                }
+            }
+        }
+        strip.show();
+    }
+}
+
+} // namespace emsesp
---
+++ LEDControlModule.h
@@ -0,0 +1,48 @@
+#ifndef LEDCONTROLMODULE_H
+#define LEDCONTROLMODULE_H
+
+#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <memory>
+#include <vector>
+#include <emsesp.h>
+
+
+#define LED_PIN 6
+
+namespace emsesp {
+
+class LEDControlModule {
+public:
+    class LEDConfig {
+    public:
+        LEDConfig(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0, bool blink = false)
+            : r(r), g(g), b(b), blink(blink) {}
+
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+        bool blink;
+    };
+
+    LEDControlModule();
+
+    void start(emsesp::EMSESP *emsesp_main, bool test_mode = false);
+    void loop();
+    void configureLED(size_t index, uint8_t r, uint8_t g, uint8_t b, bool blink);
+    void list(JsonObject output);
+
+private:
+    static constexpr int NUM_LEDS = 8;
+
+    Adafruit_NeoPixel strip;
+    std::vector<LEDConfig> ledConfigs;
+    unsigned long lastBlinkTime = 0;
+    bool blinkState = false;
+
+    void setLED(size_t index, uint8_t r, uint8_t g, uint8_t b);
+    void updateLEDs();
+    void handleBlink();
+};
+
+} // namespace emsesp
